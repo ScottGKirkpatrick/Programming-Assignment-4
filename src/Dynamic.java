da500863e5879e7871e53210d023ad93481fd103
ScottGKirkpatrick <ScottGKirkpatrick@gmail.com>
2018-04-20 14:11:27 -0600

    Initial files and comments

---
+++ src/Dynamic.java
@@ -0,0 +1,10 @@
+/*
+ * Authors: Scott Kirkpatrick
+ * Date: 4/20/18
+ * Overview: Creates change for a value from a set of coins using dynamic programming methods
+ */
+
+
+public class Dynamic {
+    
+}
